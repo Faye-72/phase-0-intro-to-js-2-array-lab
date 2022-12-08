d5efbb4348908a59e8a08cc8f7ece76115dfb1b1
faith mitei <faithmitey@gmail.com>
2022-12-08 17:08:58 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+const cats = new Array('Milo', 'Otis', 'Garfield')
+
+
+beforeEach(function () {
+      cats.length = 0;
+      cats.push('Milo', 'Otis', 'Garfield');
+    });
+
+function destructivelyAppendCat(name){
+    return (cats.push(name))
+}
+destructivelyAppendCat('Ralph')
+
+function destructivelyPrependCat(name){
+return (cats.unshift(name))
+}
+destructivelyPrependCat("Bob")
+
+function destructivelyRemoveLastCat(name){
+return (cats.pop(name))
+}
+
+destructivelyRemoveLastCat('Garfield')
+
+function destructivelyRemoveFirstCat(name){
+return (cats.shift('Millo'))
+}
+
+function appendCat(name){
+    return ([...cats , name])
+}
+appendCat(...cats, "Broom")
+appendCat(...cats)
+
+
+function prependCat(name){
+    return ([name, ...cats])
+}
+prependCat("Arnold", ...cats)
+
+function removeLastCat(){
+    return (cats.slice(0,-1))
+}
+removeLastCat()
+
+function removeFirstCat(){
+    return (cats.slice(1))
+}
